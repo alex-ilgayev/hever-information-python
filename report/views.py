0209315cd305c7db1215f935e5e1f1bdcaae7a6d
Alex <alex.ilgayev@gmail.com>
2017-10-07 20:21:39 +0300

    no message

--- report/views.py
+++ report/views.py
@@ -1,9 +1,9 @@
 from django.http import Http404
-
+from django.contrib.contenttypes.models import ContentType
 from one_report.settings import GOOGLE_TOKEN_ID
 from one_report.settings import UNIT_ID
 from one_report.settings import DATE
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
 from django.contrib.auth import authenticate, login
 from oauth2client.crypt import AppIdentityError
 from oauth2client.client import verify_id_token
@@ -52,6 +52,10 @@ import re
 
 class ReportList(APIView):
 
+    # GET request for all reports with 2 parameters:
+    # 'unit' - unit id
+    # 'date' - date in format of 10-12-2017
+    # returns report according to filters, with proper authorization.
     def get(self, request):
         # is response None, everything good. else return tuple (text, response status)
         response_val = authenticate_google_token_id(request)
@@ -63,6 +67,7 @@ class ReportList(APIView):
         chosen_date = request.GET.get(DATE)
 
         reports_returned = Report.objects.all()
+        # checking validity of parameters
         if unit_id is not None:
             if not unit_id.isdigit():
                 return Response('Enter unit number like \'unit=818\'', status=status.HTTP_400_BAD_REQUEST)
@@ -73,6 +78,7 @@ class ReportList(APIView):
 
             reports_returned = reports_returned.filter(unit=unit_id)
 
+        # checking validity of parameters
         if chosen_date is not None:
             re_result = re.search('([0-9]{2})-([0-9]{2})-([0-9]{4})', chosen_date)
             if re_result is None:
@@ -103,12 +109,29 @@ class ReportList(APIView):
         if chosen_date is not None and unit_id is not None and len(reports_returned) == 0:
 
             single_report = Report.objects.create(date=chosen_datetime, unit=chosen_unit[0])
+
+            # some glitch
+            # because I can't add permission non-programtically, checking if permission exists, and create it.
+            content_type = ContentType.objects.get_for_model(Unit)
+            Permission.objects.get_or_create(codename='can_read_unit_' + str(single_report.unit_id),
+                                                               name='Can Read Unit ' + str(single_report.unit_id),
+                                                               content_type=content_type)
+            Permission.objects.get_or_create(codename='can_update_unit_' + str(single_report.unit_id),
+                                                               name='Can Update Unit ' + str(single_report.unit_id),
+                                                               content_type=content_type)
+
             persons = Person.objects.all().filter(unit_id=unit_id)
             for person in persons:
                 ReportEntry.objects.create(person=person, status='NOT_SET', report=single_report)
             reports_returned = [single_report]
 
-        serializer = ReportSerializer(reports_returned, many=True)
+        # check permission to see.
+        permitted_reports = [report for report in reports_returned if
+                             request.user.has_perm('report.can_read_unit_' + str(report.unit_id))]
+        # permitted_reports = reports_returned
+
+
+        serializer = ReportSerializer(permitted_reports, many=True)
         return Response(serializer.data)
 
     # def post(self, request, format=None):
@@ -187,6 +210,11 @@ class ReportEntryDetail(APIView):
             return Response(response_val[0], status=response_val[1])
 
         report_entry = self.get_object(pk)
+
+        # check permission to update.
+        if not request.user.has_perm('report.can_update_unit_' + str(report_entry.report.unit_id)):
+            return Response('no permission for doing the operation', status=status.HTTP_403_FORBIDDEN)
+
         serializer = ReportEntryUpdateSerializer(report_entry, data=request.data)
         if serializer.is_valid():
             serializer.save()

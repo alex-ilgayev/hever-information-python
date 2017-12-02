96ecbec865b22c828348556c9058a06d15f24996
Alex Ilgayev <alex.ilgayev@gmail.com>
2017-12-02 16:27:36 +0200

    no message

--- geo_report/models.py
+++ geo_report/models.py
@@ -5,7 +5,10 @@ class GeoReport(models.Model):
     id = models.UUIDField(primary_key=True)
     long = models.FloatField()
     lat = models.FloatField()
-    timestamp = models.DateField()
+    timestamp = models.BigIntegerField()
+    accuracy = models.FloatField()
+    deviceId = models.TextField(max_length=30)
+
 
     def __str__(self):
         return str(self.id) + ' - ' + "long: " + str(self.long) + " lat: " + str(self.lat) + ' ' + str(self.timestamp)
